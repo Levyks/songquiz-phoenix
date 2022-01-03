af1cb0ee2ef4a39316d42b354c0c6432f2a1b0be
Levy Barbosa <marcio.levy20@gmail.com>
2022-01-03 19:18:26 -0300

    initial commit

---
+++ lib/songquiz/application.ex
@@ -0,0 +1,34 @@
+defmodule Songquiz.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      SongquizWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Songquiz.PubSub},
+      # Start the Endpoint (http/https)
+      SongquizWeb.Endpoint
+      # Start a worker by calling: Songquiz.Worker.start_link(arg)
+      # {Songquiz.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Songquiz.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    SongquizWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ lib/songquiz/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Songquiz.Mailer do
+  use Swoosh.Mailer, otp_app: :songquiz
+end
